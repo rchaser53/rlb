e55f4d93a27c74be57d65f32bfeb2c01dac5ff1a
rchaser53 <tayoshizawa29@gmail.com>
2019-11-30 15:44:29 +0900

    try to fix mutex sample using crossbeam

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,57 @@
 #[macro_use]
 extern crate crossbeam_channel;
-extern crate crossbeam_utils;
 
 use crossbeam_channel::bounded;
 use crossbeam_utils::thread;
+use std::sync::{Arc, Mutex};
 
-use std::sync::mpsc;
-// use std::thread;
+fn bar() {
+    let people = vec!["Anna", "Bob", "Cody", "Dave", "Eva"];
+    let (s, r) = bounded(1); // Make room for one unmatched send.
 
-static NTHREAD: i32 = 3;
+    let seek = |name, s, r| {
+        select! {
+            recv(r) -> peer => println!("{} recieved a message from {}.", name, peer.unwrap()),
+            send(s, name) -> _ => {}, // Wait for someone to receive my message
+        }
+    };
 
-fn main() {
-  let people = vec!["Anna", "Bob", "Cody", "Dave", "Eva"];
-  let (s, r) = bounded(1);  // Make room for one unmatched send.
+    thread::scope(|scope| {
+        for name in people {
+            let (s, r) = (s.clone(), r.clone());
+            scope.spawn(move |_| seek(name, s, r));
+        }
+    })
+    .unwrap();
 
-  let seek = |name, s, r| {
-      select! {
-          recv(r) -> peer => println!("{} recieved a message from {}.", name, peer.unwrap()),
-          send(s, name) -> _ => {}, // Wait for someone to receive my message
-      }
-  };
-
-  thread::scope(|scope| {
-      for name in people {
-          let (s, r) = (s.clone(), r.clone());
-          scope.spawn(move |_| seek(name, s, r));
-      }
-  })
-  .unwrap();
-
-  if let Ok(name) = r.try_recv() {
-      println!("No one recieved {}'s message.", name);
-  }
+    if let Ok(name) = r.try_recv() {
+        println!("No one recieved {}'s message.", name);
+    }
 }
 
-// fn foo() {
-//     let (tx, rx) = mpsc::channel();
-//     let mut children = vec![];
+static NTHREAD: i32 = 3;
+fn main() {
+    let (tx, rx) = bounded(NTHREAD as usize);
+    let children = Arc::new(Mutex::new(vec![]));
 
-//     for id in 0..NTHREAD {
-//         let thread_tx = tx.clone();
+    for id in 0..NTHREAD {
+        let thread_tx = tx.clone();
+        let cloned = children.clone();
+        thread::scope(|scope| {
+            let _ = scope.spawn(move |_| {
+                thread_tx.send(id).unwrap();
+                cloned.lock().unwrap().push(id);
+                println!("thread {} finished", id);
+            });
+        })
+        .unwrap();
+    }
 
-//         thread::scope(|scope| {
-//             let child = scope.spawn(move |_| {
-//                 thread_tx.send(id).unwrap();
-//                 println!("thread {} finished", id);
-//             });
-//             children.push(child);
-//         }).unwrap();
-//         // let child = thread::spawn(move || {
-//         //     thread_tx.send(id).unwrap();
-//         //     println!("thread {} finished", id);
-//         // });
-//     }
+    let mut ids = Vec::with_capacity(NTHREAD as usize);
+    for _ in 0..NTHREAD {
+        ids.push(rx.recv());
+    }
 
-//     let mut ids = Vec::with_capacity(NTHREAD as usize);
-//     for _ in 0..NTHREAD {
-//         ids.push(rx.recv());
-//     }
-
-//     for child in children {
-//         child.join().expect("the child thread is paniced");
-//     }
-
-//     // Show the order in whicn the messages were sent
-//     println!("{:?}", ids);
-// }
+    println!("children: {:?}", children);
+    println!("ids: {:?}", ids);
+}

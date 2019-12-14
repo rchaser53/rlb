6be6041bd4978d6f7d4d126f627bc2ea1dbeac94
rchaser53 <tayoshizawa29@gmail.com>
2019-12-14 15:15:35 +0900

    create req uitl mod

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,22 @@
-use actix_web::client::{Client, ClientRequest};
-use actix_web::{dev, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use std::net::ToSocketAddrs;
-use url::Url;
+use actix_web::client::Client;
+use actix_web::{
+    middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+};
 
-// TODO: This forwarded implementation is incomplete as it only handles the inofficial
-// X-Forwarded-For header but not the official Forwarded one.
-fn create_forwarded_req(
-    client: &web::Data<Client>,
-    head: &dev::RequestHead,
-    new_url: &str,
-) -> ClientRequest {
-    let forwarded_req = client.request_from(new_url, head).no_decompress();
-    if let Some(addr) = head.peer_addr {
-        forwarded_req.header("x-forwarded-for", format!("{}", addr.ip()))
-    } else {
-        forwarded_req
-    }
-}
+mod req;
+use crate::req::{create_forward_url, create_forwarded_req};
 
 pub async fn forward(
     req: HttpRequest,
     body: web::Bytes,
-    url: web::Data<Url>,
     client: web::Data<Client>,
 ) -> Result<HttpResponse, Error> {
-    let mut new_url = url.get_ref().clone();
-    new_url.set_path(req.uri().path());
-    new_url.set_query(req.uri().query());
-
+    let original_url = req.uri();
     let head = req.head();
-    let forwarded_req = create_forwarded_req(&client, head, new_url.as_str());
+    let host = "127.0.0.1";
 
+    let new_url = create_forward_url(&original_url, host, 8080);
+    let forwarded_req = create_forwarded_req(&client, head, new_url.as_str());
     let mut res = match forwarded_req.send_body(body.clone()).await {
         Ok(res) => res,
         Err(err) => {
@@ -58,20 +44,10 @@ pub async fn main() -> std::io::Result<()> {
 
     let proxy_addr = "127.0.0.1";
     let proxy_port = 3000;
-    let forward_url = Url::parse(&format!(
-        "http://{}",
-        ("127.0.0.1", 8080)
-            .to_socket_addrs()
-            .unwrap()
-            .next()
-            .unwrap()
-    ))
-    .unwrap();
 
     HttpServer::new(move || {
         App::new()
             .data(Client::new())
-            .data(forward_url.clone())
             .wrap(middleware::Logger::default())
             .default_service(web::route().to(forward))
     })
